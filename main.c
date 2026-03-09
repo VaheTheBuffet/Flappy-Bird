1228cff048497e008101dcbdcbb50f5aaab7f5aa
Vahe <vpetrosyan1342@gmail.com>
2026-03-09 03:49:07 -0700

    much more polished now

--- main.c
+++ main.c
@@ -1,32 +1,9 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include "main.h"
 
-#define APPLICATION_NAME "game"
-#define WIDTH 800
-#define HEIGHT 600
-#define HORIZONTAL_SPEED 100 
-#define VERTICAL_SPEED 250
-#define PIPE_GAP 200
-#define PIPE_WIDTH 100
-#define PLAYER_WIDTH 100
-#define PIPE_INTERVAL (PIPE_WIDTH + WIDTH) / 2
-#define NUM_PIPES 2
-#define FIRST_PIPE_X 400.0
-
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
-
-static double SECOND = 1.0;
-static double FPS = 0.0;
-static double START_FRAME_T = 0.0;
-static double DELTA = 0.0;
-
-//STATE TODO: maybe use the appstate
-static Player PLAYER;
-static Pipe PIPES[NUM_PIPES];
-static SDL_FRect PIPE_MESHES[NUM_PIPES * 2];
-static SDL_FRect PLAYER_MESH;
-static SDL_Texture *TEST_TEXTURE;
+static GameState GAMESTATE;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -43,21 +20,18 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     }
     SDL_SetRenderLogicalPresentation(renderer, WIDTH, HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
-    SDL_Surface *tex_data = SDL_LoadPNG( "./assets/test.png");
-    if (!tex_data)
-    {
-        printf("could not load texture file");
+    if(
+        !load_texture("./assets/test.png", &GAMESTATE.textures.test)
+        || !load_texture("./assets/pipe.png", &GAMESTATE.textures.pipe)
+        || !load_texture("./assets/bird.png", &GAMESTATE.textures.bird)
+        || !load_texture("./assets/background.png", &GAMESTATE.textures.background)
+        || !load_texture("./assets/clouds.png", &GAMESTATE.textures.clouds)
+    ) {
         return SDL_APP_FAILURE;
     }
-    TEST_TEXTURE = SDL_CreateTextureFromSurface(renderer, tex_data);
-    if (!TEST_TEXTURE)
-    {
-        printf("could not create texture");
-        return SDL_APP_FAILURE;
-    }
-    SDL_DestroySurface(tex_data);
 
-    game_init();
+    game_init(&GAMESTATE);
+    *appstate = (void*)&GAMESTATE;
 
     return SDL_APP_CONTINUE;
 }
@@ -72,28 +46,27 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    START_FRAME_T = SDL_GetTicks();
+    GameState* game_state = (GameState *) appstate;
+    game_state->start_frame_t = SDL_GetTicks();
 
-    SDL_SetRenderDrawColorFloat(renderer, 0.1, 0.2, 0.8, SDL_ALPHA_OPAQUE_FLOAT);
+    SDL_SetRenderDrawColorFloat(renderer, 0.1, 0.5, 0.8, SDL_ALPHA_OPAQUE_FLOAT);
     SDL_RenderClear(renderer);
 
-    if (!update(DELTA))
-    {
+    if (!update(game_state->delta_t, game_state)) {
         printf("You lost!\n");
         return SDL_APP_SUCCESS;
     };
 
-    draw();
-    SDL_RenderTexture(renderer, TEST_TEXTURE, NULL, &PLAYER_MESH);
+    draw(game_state);
+    SDL_RenderDebugTextFormat(renderer, WIDTH - 200, 50, "%f FPS", game_state->fps);
     SDL_RenderPresent(renderer);
 
-    DELTA = ((double)SDL_GetTicks() - START_FRAME_T) / 1000.0;
-    SECOND -= DELTA;
+    game_state->delta_t = ((double)SDL_GetTicks() - game_state->start_frame_t) / 1000.0;
+    game_state->second -= game_state->delta_t;
 
-    if (SECOND < 0)
-    {
-        FPS = 1.0 / DELTA;
-        SECOND = 1.0;
+    if (game_state->second < 0) {
+        game_state->fps = 1.0 / game_state->delta_t;
+        game_state->second = 1.0;
     }
 
     return SDL_APP_CONTINUE;
@@ -101,33 +74,66 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
+    GameState *state = (GameState*)appstate;
+    SDL_DestroyTexture(state->textures.test);
+    SDL_DestroyTexture(state->textures.pipe);
+    SDL_DestroyTexture(state->textures.bird);
+    SDL_DestroyTexture(state->textures.background);
 }
 
-//------------------- GAME LOGIC -----------------------
-
-void game_init() 
+bool load_texture(const char* path, SDL_Texture **tex) 
 {
-    PLAYER = (Player){0.0, 0.0, HORIZONTAL_SPEED, -VERTICAL_SPEED};
-    PIPES[0] = (Pipe){FIRST_PIPE_X, (float)(rand() % (HEIGHT - PIPE_GAP - 5))};
-    PIPES[1] = (Pipe){FIRST_PIPE_X + PIPE_INTERVAL, (float)(rand() % (HEIGHT - PIPE_GAP - 5))};
+    SDL_Surface *tex_data = SDL_LoadPNG(path);
+    if (!tex_data) {
+        printf("could not load texture file");
+        return false;
+    }
+    
+    *tex = SDL_CreateTextureFromSurface(renderer, tex_data);
+    if (!*tex) {
+        printf("could not create texture");
+        return false;
+    }
+
+    SDL_DestroySurface(tex_data);
+    return true;
+}
+
+//--------------------- GAME LOGIC --------------------------//
+
+void game_init(GameState *state) 
+{
+    state->second = 1.0;
+    state->fps = 0.0;
+    state->start_frame_t = 0.0;
+    state->delta_t = 0.0;
+    state->player = (Player){0.0, 0.0, HORIZONTAL_SPEED, -VERTICAL_SPEED};
+    state->pipes[0] = (Pipe){FIRST_PIPE_X, (float)(rand() % (HEIGHT - PIPE_GAP - 5))};
+    state->pipes[1] = (Pipe){FIRST_PIPE_X + PIPE_INTERVAL, (float)(rand() % (HEIGHT - PIPE_GAP - 5))};
+
+    state->meshes.background = (SDL_FRect) {
+        0.0, 0.0,
+        WIDTH, HEIGHT
+    };
+
+    state->meshes.clouds = (SDL_FRect) {
+        0.0, 0.0,
+        WIDTH, HEIGHT / 2
+    };
 }
 
 //Returns false on game over
-bool update(double dt)
-{
-    for(size_t i = 0; i < NUM_PIPES; i++) 
-    {
-        if (collision(PIPES + i, &PLAYER)) 
-        {
+bool update(double dt, GameState *state) {
+    for(size_t i = 0; i < NUM_PIPES; i++) {
+        if (collision(state->pipes + i, &state->player)) {
             return false;
         }
     }
     //PIPES[0] is always the earlier pipe
-    if (PLAYER.x - PIPES[0].x > PIPE_INTERVAL)
-    {
-        PIPES[0] = PIPES[1];
-        PIPES[1] = (Pipe){
-            PIPES[0].x + PIPE_INTERVAL, 
+    if (state->player.x - state->pipes[0].x > PIPE_INTERVAL) {
+        state->pipes[0] = state->pipes[1];
+        state->pipes[1] = (Pipe){
+            state->pipes[0].x + PIPE_INTERVAL, 
             //5 is an offset so the pipe isn't at the very edge of the screen
             (float)(rand() % (HEIGHT - PIPE_GAP - 5)) 
         };
@@ -135,45 +141,78 @@ bool update(double dt)
 
     const bool *key_states = SDL_GetKeyboardState(NULL);
 
-    if (key_states[SDL_SCANCODE_W]) 
-    {
-        PLAYER.vy = VERTICAL_SPEED;
+    if (key_states[SDL_SCANCODE_W]) {
+        state->player.vy = VERTICAL_SPEED;
     }
-    if (key_states[SDL_SCANCODE_S]) 
-    {
-        PLAYER.vy = -VERTICAL_SPEED;
+    if (key_states[SDL_SCANCODE_S]) {
+        state->player.vy = -VERTICAL_SPEED;
     }
-    if (key_states[SDL_SCANCODE_A]) 
-    {
-        PLAYER.vx = -HORIZONTAL_SPEED;
+    if (key_states[SDL_SCANCODE_A]) {
+        state->player.vx = -HORIZONTAL_SPEED;
     }
-    if (key_states[SDL_SCANCODE_D]) 
-    {
-        PLAYER.vx = HORIZONTAL_SPEED;
+    if (key_states[SDL_SCANCODE_D]) {
+        state->player.vx = HORIZONTAL_SPEED;
     }
 
-    PLAYER.y -= PLAYER.vy * dt;
-    PLAYER.x += PLAYER.vx * dt;
-    PLAYER.vy += (-VERTICAL_SPEED - PLAYER.vy) * dt * 3;
-    PLAYER.vx = 0;
+    state->player.y -= state->player.vy * dt;
+    state->player.x += state->player.vx * dt;
+    state->player.vy += (-VERTICAL_SPEED - state->player.vy) * dt * 3;
+    state->player.vx = 0;
 
     return true;
 }
 
-void draw() 
+void draw(GameState *state) 
 {
-    Player_get_mesh(&PLAYER, &PLAYER_MESH);
-    for(size_t i=0; i < NUM_PIPES; i++) 
-    {
-        Pipe_get_mesh(PIPES + i, &PLAYER, PIPE_MESHES + 2*i, PIPE_MESHES + 2*i + 1);
+    SDL_RenderTexture(
+        renderer, 
+        state->textures.background, 
+        &(SDL_FRect) {
+            (int)state->player.x % state->textures.background->w/2, 
+            0.0, 
+            state->textures.background->w / 3, 
+            state->textures.background->h}, 
+        &state->meshes.background);
+
+    SDL_RenderTexture(
+        renderer, 
+        state->textures.clouds, 
+        &(SDL_FRect) {
+            (int)state->player.x % state->textures.background->w/2, 
+            0.0, 
+            state->textures.background->w / 4, 
+            state->textures.background->h}, 
+        &state->meshes.clouds);
+
+    Player_get_mesh(&state->player, &state->meshes.player);
+    for(size_t i=0; i < NUM_PIPES; i++) {
+        Pipe_get_mesh(
+            state->pipes + i, 
+            &state->player, 
+            state->meshes.pipes + 2*i, 
+            state->meshes.pipes + 2*i + 1);
     }
 
-    SDL_SetRenderDrawColorFloat(renderer, 1.0, 0.0, 0.0, SDL_ALPHA_OPAQUE_FLOAT);
-    SDL_RenderTexture(renderer, TEST_TEXTURE, NULL, &PLAYER_MESH);
-    SDL_SetRenderDrawColorFloat(renderer, 0.0, 1.0, 0.0, SDL_ALPHA_OPAQUE_FLOAT);
-    for(size_t i=0; i < NUM_PIPES * 2; i++) 
-    {
-        SDL_RenderTexture(renderer, TEST_TEXTURE, NULL, PIPE_MESHES + i);
+    SDL_SetRenderDrawColorFloat(renderer, 0.6, 0.8, 0.99, SDL_ALPHA_OPAQUE_FLOAT);
+    SDL_RenderTextureRotated(
+            renderer, 
+            state->textures.bird, 
+            NULL, 
+            &state->meshes.player,
+            CLAMP(-state->player.vy / 8, -90, 90),
+            NULL,
+            SDL_FLIP_NONE);
+
+    SDL_SetRenderDrawColorFloat(renderer, 0.0, 0.0, 0.0, SDL_ALPHA_OPAQUE_FLOAT);
+    for(size_t i=0; i < NUM_PIPES * 2; i++) {
+        SDL_RenderTextureRotated(
+            renderer, 
+            state->textures.pipe, 
+            NULL,
+            state->meshes.pipes + i,
+            0,
+            NULL,
+            !(i&1) ? SDL_FLIP_VERTICAL : SDL_FLIP_NONE);
     }
 }
 
@@ -191,8 +230,7 @@ bool Pipe_get_mesh(Pipe *pipe, Player *player, SDL_FRect *mesh_top, SDL_FRect *m
 {
     //If the top pipe is so large there isn't enough space to construct the bttom pipe
     //with adequate gap
-    if(pipe->height > HEIGHT - PIPE_GAP) 
-    {
+    if(pipe->height > HEIGHT - PIPE_GAP) {
         return false;
     }
 
@@ -215,7 +253,7 @@ bool collision(Pipe *pipe, Player *player)
         && player->x < pipe->x + PIPE_WIDTH;
 
     bool y_collision = player->y < pipe->height 
-        || player->y + PLAYER_WIDTH > pipe->height + PIPE_GAP;
+        || player->y + PLAYER_HEIGHT > pipe->height + PIPE_GAP;
 
     return x_collision && y_collision;
 }
--- main.h
+++ main.h
@@ -6,6 +6,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MIN(a, b) ((a < b) ? a : b)
+#define MAX(a, b) ((a > b) ? a : b)
+#define CLAMP(x, a, b) (MIN(MAX(a, x), b))
+
+#define APPLICATION_NAME "Flappy Bird"
+#define WIDTH 800
+#define HEIGHT 600
+#define HORIZONTAL_SPEED 100 
+#define VERTICAL_SPEED 250
+#define PIPE_GAP 200
+#define PIPE_WIDTH 100
+#define BACKGROUND_TEXTURE_WIDTH 512
+#define BACKGROUND_PARALAX_RATE 3 // number of screens
+#define PLAYER_SPRITE_WIDTH 128
+#define PLAYER_WIDTH 100
+#define PLAYER_HEIGHT (68 * 100) / PLAYER_SPRITE_WIDTH
+#define PIPE_INTERVAL (PIPE_WIDTH + WIDTH) / 2
+#define NUM_PIPES 2
+#define FIRST_PIPE_X 400.0
+
 typedef struct {
     float x;
     float y;
@@ -13,19 +33,36 @@ typedef struct {
     float vy;
 } Player;
 
-bool Player_get_mesh(Player *player, SDL_FRect *mesh);
-
 typedef struct {
     float x;
     float height;
 } Pipe;
 
+typedef struct {
+    SDL_FRect player;
+    SDL_FRect pipes[4];
+    SDL_FRect background;
+    SDL_FRect clouds;
+} MeshState;
+
+typedef struct {
+    SDL_Texture *test, *bird, *background, *pipe, *clouds;
+} TextureState;
+
+typedef struct {
+    Player player;
+    Pipe pipes[2];
+    double fps, second, start_frame_t, delta_t;
+    MeshState meshes;
+    TextureState textures;
+} GameState;
+
+bool load_texture(const char* path, SDL_Texture **tex); 
+bool Player_get_mesh(Player *player, SDL_FRect *mesh);
 bool Pipe_get_mesh(Pipe* self, Player *player, SDL_FRect *mesh_top, SDL_FRect *mesh_bottom);
-
 bool collision(Pipe* pipe, Player* player);
-
-void game_init();
-void draw();
-bool update(double dt);
+void game_init(GameState *state);
+void draw(GameState *state);
+bool update(double dt, GameState* state);
 
 #endif
